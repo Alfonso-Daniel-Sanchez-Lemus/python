c3684db7ef61387829bf75c7a9f0d9d729484771
Alfonso Daniel Sanchez Lemus <68023577+Alfonso-Daniel-Sanchez-Lemus@users.noreply.github.com>
2024-10-29 23:01:06 +0000

    aaaaa

---
+++ dic.py
@@ -0,0 +1,4 @@
+usuario={"nombre":"juan","apellido":"perez","edad":30}
+print(usuario["nombre"])
+usuario["profesion"]="desarrollador de software"
+print(usuario)
---
+++ pruebas.py
@@ -0,0 +1,12 @@
+lista=[1,4,5,6,"aaaa"]
+lista.remove(4)
+print(lista)
+
+lista=[1,4,5,6,"aaaa"]
+lista.pop(3)
+print(lista)
+
+lista=[1,4,5,6,"aaaa"]
+lista.extend([2.3,2])
+print(lista)
+
